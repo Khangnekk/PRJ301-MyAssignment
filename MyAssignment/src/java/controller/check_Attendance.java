f1bb5c020d0ed32a525b8b1d340a5fa376043e98
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-14 22:09:37 +0700

    Create check_Attendance.java

---
+++ MyAssignment/src/java/controller/check_Attendance.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dal.StudentDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Student;
+
+/**
+ *
+ * @author Khangnekk
+ */
+public class check_Attendance extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        StudentDBContext db = new StudentDBContext();
+//        ArrayList<Student> list = db.list();
+//        req.setAttribute("students", list);
+        req.getRequestDispatcher("add/check_Attendance.jsp").forward(req, resp);
+    }
+}
