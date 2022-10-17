456d3dc1e8b0f5d592ba6e88b8e45691f2824b42
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-17 23:31:21 +0700

    test something (commented)

--- MyAssignment/src/java/controller/auth/LoginController.java
+++ MyAssignment/src/java/controller/auth/LoginController.java
@@ -2,12 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.auth;
 
 import dal.AccountDBContext;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,16 +17,16 @@ import model.Account;
  * @author sonnt
  */
 public class LoginController extends HttpServlet {
-   
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.getRequestDispatcher("view/lecturer/auth/login.jsp").forward(request, response);
-    } 
-    
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String email = request.getParameter("email").trim().toLowerCase();
         String password = request.getParameter("password");
         AccountDBContext db = new AccountDBContext();
@@ -45,4 +43,25 @@ public class LoginController extends HttpServlet {
             request.getRequestDispatcher("/view/lecturer/home.jsp").forward(request, response);
         }
     }
+
+//    @Override
+//    protected void processPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String email = request.getParameter("email").trim().toLowerCase();
+//        String password = request.getParameter("password");
+//        AccountDBContext db = new AccountDBContext();
+//        Account account = db.get(email, password);
+//        if (account == null) {
+////            response.getWriter().println("login failed!");
+//            request.setAttribute("noti", "* Incorrect email or password");
+//            request.getRequestDispatcher("view/lecturer/auth/login.jsp").forward(request, response);
+//        } else {
+//            request.getSession().setAttribute("account", account);
+//            request.getRequestDispatcher("/view/lecturer/home.jsp").forward(request, response);
+//        }
+//    }
+//
+//    @Override
+//    protected void processGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("view/lecturer/auth/login.jsp").forward(request, response);
+//    }
 }
