b0a6ef41da23e2718c501a17c00adddc428dcf59
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-17 23:26:47 +0700

    declare variable for lecturer

--- MyAssignment/src/java/model/Lecturer.java
+++ MyAssignment/src/java/model/Lecturer.java
@@ -4,10 +4,74 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author Khangnekk
  */
 public class Lecturer {
-    
+    private int leid;
+    private String lename;
+    private String leemail;
+    private boolean gender;
+    private Date ledob;
+    private String lephone;
+    private String leaddress;
+
+    public int getLeid() {
+        return leid;
+    }
+
+    public void setLeid(int leid) {
+        this.leid = leid;
+    }
+
+    public String getLename() {
+        return lename;
+    }
+
+    public void setLename(String lename) {
+        this.lename = lename;
+    }
+
+    public String getLeemail() {
+        return leemail;
+    }
+
+    public void setLeemail(String leemail) {
+        this.leemail = leemail;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getLedob() {
+        return ledob;
+    }
+
+    public void setLedob(Date ledob) {
+        this.ledob = ledob;
+    }
+
+    public String getLephone() {
+        return lephone;
+    }
+
+    public void setLephone(String lephone) {
+        this.lephone = lephone;
+    }
+
+    public String getLeaddress() {
+        return leaddress;
+    }
+
+    public void setLeaddress(String leaddress) {
+        this.leaddress = leaddress;
+    }
 }
