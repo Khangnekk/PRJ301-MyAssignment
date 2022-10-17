3ad2e33e2cf63a0d3ceb9f6e597f792f7d1a74ad
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-17 23:29:10 +0700

    add check login

---
+++ MyAssignment/src/java/controller/lecturer/check_Attendance.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.lecturer;
+
+import controller.auth.BaseAuthenticationController;
+import dal.StudentDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Student;
+
+/**
+ *
+ * @author Khangnekk
+ */
+public class check_Attendance extends BaseAuthenticationController{
+
+    @Override
+    protected void processPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void processGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        processRequest(req, resp);
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        processRequest(req, resp);
+//    }
+//
+    void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        StudentDBContext db = new StudentDBContext();
+//        ArrayList<Student> list = db.list();
+//        req.setAttribute("students", list);
+        req.getRequestDispatcher("add/check_Attendance.jsp").forward(req, resp);
+    }
+}
