964964d9687a05c54e23a056f3d6d13a92f6137c
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-23 23:19:14 +0700

    Create model feature and role

--- MyAssignment/src/java/model/Role.java
+++ MyAssignment/src/java/model/Role.java
@@ -4,10 +4,49 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Khangnekk
  */
 public class Role {
+    private int id;
+    private String name;
+    private ArrayList<Account> accounts = new ArrayList<>();
+    private ArrayList<Feature> features = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(ArrayList<Feature> features) {
+        this.features = features;
+    }
+    
     
 }
