71bfdfa9a39af5ee1ed0a8d54d59301cc4f321d7
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-24 04:03:41 +0700

    Create list() => get a list timeslot from db

--- MyAssignment/src/java/dal/TimeSlotDBContext.java
+++ MyAssignment/src/java/dal/TimeSlotDBContext.java
@@ -4,7 +4,14 @@
  */
 package dal;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Lecturer;
 import model.TimeSlot;
 
 /**
@@ -14,7 +21,27 @@ import model.TimeSlot;
 public class TimeSlotDBContext extends DBContext<TimeSlot>{
 
     public ArrayList<TimeSlot> list() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ArrayList<TimeSlot> timeslots = new ArrayList<>();
+        String sql = "SELECT * FROM TimeSlot";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                TimeSlot t = new TimeSlot();
+                int id = rs.getInt("tid");
+                String name = rs.getString("tname");
+                String description = rs.getString("description");
+                t.setId(id);
+                t.setName(name);
+                t.setDescription(description);
+                timeslots.add(t);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return timeslots;
     }
 
     @Override
