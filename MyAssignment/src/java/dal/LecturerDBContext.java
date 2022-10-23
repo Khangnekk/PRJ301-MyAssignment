8c0fb69462fda1d612dd130c5ebeb5339a65e2ac
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-23 23:14:50 +0700

    extends DBContext and Create Lecturer get(int lid)

--- MyAssignment/src/java/dal/LecturerDBContext.java
+++ MyAssignment/src/java/dal/LecturerDBContext.java
@@ -4,10 +4,56 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Lecturer;
+
 /**
  *
  * @author Khangnekk
  */
-public class LecturerDBContext {
-    
+public class LecturerDBContext extends DBContext<Lecturer> {
+
+    public Lecturer get(int lid) {
+        try {
+            String sql = "SELECT leid,lename FROM Lecturer WHERE leid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, lid);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Lecturer l = new Lecturer();
+                l.setId(rs.getInt("leid"));
+                l.setName(rs.getString("lename"));
+                return l;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LecturerDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void insert(Lecturer model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Lecturer model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Lecturer model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Lecturer> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
