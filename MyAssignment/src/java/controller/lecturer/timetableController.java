4f537c78ee87c750a0be9846640eab73157b0eac
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-23 22:10:34 +0700

    Create DBContext for model, Create DateTimeHelper
    TimeSlotDBContext
    SessionDBContext
    LecturerDBContext

--- MyAssignment/src/java/controller/lecturer/timetableController.java
+++ MyAssignment/src/java/controller/lecturer/timetableController.java
@@ -5,14 +5,19 @@
 package controller.lecturer;
 
 import controller.auth.lecturer.BaseAuthenticationController;
-import dal.StudentDBContext;
+import dal.LecturerDBContext;
+import dal.SessionDBContext;
+import dal.TimeSlotDBContext;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import model.Student;
+import java.util.Date;
+import model.Lecturer;
+import model.Session;
+import model.TimeSlot;
+import util.DateTimeHelper;
 
 /**
  *
@@ -20,17 +25,42 @@ import model.Student;
  */
 public class timetableController extends BaseAuthenticationController {
 
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        processRequest(req, resp);
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        processRequest(req, resp);
-//    }
-
     void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int lid = Integer.parseInt(req.getParameter("lid"));
+        String raw_from = req.getParameter("from");
+        String raw_to = req.getParameter("to");
+        java.sql.Date from = null;
+        java.sql.Date to = null;
+        if(raw_from ==null || raw_from.length() ==0)
+        {
+            Date today = new Date();
+            int todayOfWeek = DateTimeHelper.getDayofWeek(today);
+            Date e_from = DateTimeHelper.addDays(today, 2 - todayOfWeek);
+            Date e_to = DateTimeHelper.addDays(today, 8-todayOfWeek);
+            from = DateTimeHelper.toDateSql(e_from);
+            to = DateTimeHelper.toDateSql(e_to);
+        }
+        else
+        {
+            from = java.sql.Date.valueOf(raw_from);
+            to = java.sql.Date.valueOf(raw_to);
+        }
+        
+        req.setAttribute("from", from);
+        req.setAttribute("to", to);
+        req.setAttribute("dates", DateTimeHelper.getDateList(from, to));
+        
+        TimeSlotDBContext slotDB = new TimeSlotDBContext();
+        ArrayList<TimeSlot> slots = slotDB.list();
+        req.setAttribute("slots", slots);
+        
+        SessionDBContext sesDB = new SessionDBContext();
+        ArrayList<Session> sessions = sesDB.filter(lid, from, to);
+        req.setAttribute("sessions", sessions);
+        
+        LecturerDBContext lecDB = new LecturerDBContext();
+        Lecturer lecturer = lecDB.get(lid);
+        req.setAttribute("lecturer", lecturer);
         req.getRequestDispatcher("view/lecturer/timeTable.jsp").forward(req, resp);
     }
 
