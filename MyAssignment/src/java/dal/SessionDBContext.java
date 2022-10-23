9afa9946093f6d3719f47b7a49f574eaf7289765
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-23 23:15:38 +0700

    extends DBContext and ArrayList<Session> filter(int lid, Date from, Date to)

--- MyAssignment/src/java/dal/SessionDBContext.java
+++ MyAssignment/src/java/dal/SessionDBContext.java
@@ -4,10 +4,112 @@
  */
 package dal;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.*;
 /**
  *
  * @author Khangnekk
  */
-public class SessionDBContext {
-    
+public class SessionDBContext extends DBContext<Session> {
+
+    public ArrayList<Session> filter(int lid, Date from, Date to) {
+
+        ArrayList<Session> sessions = new ArrayList<>();
+        try {
+            String sql_get_Sesion = "SELECT \n"
+                    + "ses.seid,ses.[date],ses.[index],ses.attend,\n"
+                    + "l.leid,l.lename,\n"
+                    + "g.gid,g.gname,\n"
+                    + "sub.subid,sub.subname,\n"
+                    + "r.rid,r.rname,\n"
+                    + "t.tid,t.[description]\n"
+                    + "FROM [Session] ses\n"
+                    + "INNER JOIN Lecturer l ON l.leid = ses.leid\n"
+                    + "INNER JOIN [Group] g ON g.gid = ses.gid\n"
+                    + "INNER JOIN [Subject] sub ON sub.subid = g.subid\n"
+                    + "INNER JOIN Room r ON r.rid = ses.rid\n"
+                    + "INNER JOIN TimeSlot t ON t.tid = ses.tid\n"
+                    + "WHERE\n"
+                    + "l.leid = 1\n"
+                    + "AND ses.[date] >= ?\n"
+                    + "AND ses.[date] <= ?";
+            PreparedStatement stm = connection.prepareStatement(sql_get_Sesion);
+            stm.setInt(1, lid);
+            stm.setDate(2, from);
+            stm.setDate(3, to);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next())
+            {
+                Session session = new Session();
+                Lecturer l = new Lecturer();
+                Room r = new Room();
+                Group g = new Group();
+                TimeSlot t = new TimeSlot();
+                Subject sub = new Subject();
+                
+                session.setId(rs.getInt("seid"));
+                session.setDate(rs.getDate("date"));
+                session.setIndex(rs.getInt("index"));
+                session.setAttendated(rs.getBoolean("attend"));
+                
+                l.setId(rs.getInt("leid"));
+                l.setName(rs.getString("lename"));
+                session.setLecturer(l);
+                
+                g.setId(rs.getInt("gid"));
+                g.setName(rs.getString("gname"));
+                session.setGroup(g);
+                
+                sub.setId(rs.getInt("subid"));
+                sub.setName(rs.getString("subname"));
+                g.setSubject(sub);
+                
+                r.setId(rs.getInt("rid"));
+                r.setName(rs.getString("rname"));
+                session.setRoom(r);
+                
+                t.setId(rs.getInt("tid"));
+                t.setName("tname");
+                t.setDescription(rs.getString("description"));
+                session.setTimeslot(t);
+                
+                sessions.add(session);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sessions;
+    }
+
+    @Override
+    public void insert(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Session get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Session> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
