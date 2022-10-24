6417ffcbcf3528637cec9db6c7b98f128e1a0d6c
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-24 13:53:56 +0700

    create variable, getter,setter

--- MyAssignment/src/java/model/Attendance.java
+++ MyAssignment/src/java/model/Attendance.java
@@ -9,5 +9,51 @@ package model;
  * @author Khangnekk
  */
 public class Attendance {
+    private int id;
+    private boolean present;
+    private String description;
+    private Student student;
+    private Session session;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+    
     
 }
