7903c6a1a3d0d4227eafe5b11e7017b7828d7771
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-24 11:14:46 +0700

    add var

--- MyAssignment/src/java/model/Major.java
+++ MyAssignment/src/java/model/Major.java
@@ -4,10 +4,40 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Khangnekk
  */
 public class Major {
+    private int id;
+    private String name;
+    private Student student;
+    ArrayList<Student> students = new ArrayList<>();
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
     
 }
