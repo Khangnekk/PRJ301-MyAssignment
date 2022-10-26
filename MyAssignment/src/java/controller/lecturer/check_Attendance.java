4158796363dd6042735463de9bb2d90cd4a084f2
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-10-26 17:50:49 +0700

    get sessionScope and requestScope

--- MyAssignment/src/java/controller/lecturer/check_Attendance.java
+++ MyAssignment/src/java/controller/lecturer/check_Attendance.java
@@ -5,11 +5,17 @@
 package controller.lecturer;
 
 import controller.auth.lecturer.BaseAuthenticationController;
+import dal.AttendanceDBContext;
+import dal.GroupDBContext;
+import dal.LecturerDBContext;
 import dal.SessionDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import model.Attendance;
+import model.Group;
 import model.Session;
 
 /**
@@ -29,10 +35,30 @@ public class check_Attendance extends BaseAuthenticationController {
     }
 
     void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {    
-        int seid = Integer.parseInt(req.getParameter("seid"));
+        
+        LecturerDBContext lecturerDB = new LecturerDBContext();
+        GroupDBContext groupDB = new GroupDBContext();
         SessionDBContext sesDB = new SessionDBContext();
-        Session getSessionsByID = sesDB.get(seid);
-        req.getSession().setAttribute("getSessionsByID", getSessionsByID);
+        AttendanceDBContext attDB = new AttendanceDBContext();
+        Group group;
+        
+        String email = req.getParameter("email");
+        int gid = Integer.parseInt(req.getParameter("gid"));
+        int lid = lecturerDB.getIdByEmail(email);
+        int seid = Integer.parseInt(req.getParameter("seid"));
+        
+        ArrayList<Group> groupss = groupDB.listGroupByLeid(lid);
+        ArrayList<Session> sessionsByGidAndLeid = sesDB.getSessionByGidAndLeid(lid, gid);
+        ArrayList<Attendance> attendances = attDB.getAttendancesBySeid(seid);
+        
+        group = groupDB.get(gid);
+        
+        req.getSession().setAttribute("sessionsByGidAndLeid", sessionsByGidAndLeid);
+        req.setAttribute("email", email);
+        req.setAttribute("attendances", attendances);
+        req.getSession().setAttribute("group", group);
+        req.getSession().setAttribute("groupss", groupss);
+
         req.getRequestDispatcher("add/check_Attendance.jsp").forward(req, resp);
     }
 }
