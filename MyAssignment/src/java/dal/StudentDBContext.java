7e049803a8fdcedc5d0a64ad78ad28aeb74d6472
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-11-05 16:48:39 +0700

    add method getIdByEmail(Need to provide email information) , get(Need to provide student id information)

--- MyAssignment/src/java/dal/StudentDBContext.java
+++ MyAssignment/src/java/dal/StudentDBContext.java
@@ -36,8 +36,44 @@ public class StudentDBContext extends DBContext<Student> {
     }
 
     @Override
-    public Student get(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Student get(int studentid) {
+        MajorDBContext majDB = new MajorDBContext();
+        ArrayList<Major> majors = majDB.list();
+
+        String sql = "SELECT * FROM Student WHERE stuid = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, studentid);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Student s = new Student();
+
+                int id = rs.getInt("stuid");
+                String name = rs.getString("stuname");
+                String email = rs.getString("stuemail");
+                boolean gender = rs.getBoolean("stugender");
+                Date dob = rs.getDate("studob");
+                String phone = rs.getString("stuphone");
+                String address = rs.getString("stuaddress");
+                int mid = rs.getInt("mid");
+
+                s.setId(id);
+                s.setName(name);
+                s.setEmail(email);
+                s.setGender(gender);
+                s.setDob(dob);
+                s.setPhone(phone);
+                s.setAddress(address);
+
+                s.setMid(majors.stream().filter(m -> m.getId() == mid).findAny().get());
+                return s;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
     }
 
     @Override
@@ -79,6 +115,21 @@ public class StudentDBContext extends DBContext<Student> {
         }
 
         return students;
+    }
+    
+    public int getIdByEmail(String stuemail){
+        try {
+            String sql = "SELECT stuid FROM Student WHERE stuemail = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, stuemail);
+            ResultSet rs = stm.executeQuery();
+            if(rs.next()){
+                return rs.getInt("stuid");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LecturerDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
     }
 
 }
