8f8b530969b169532a75daf5a73377e86f98f4ea
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2022-11-06 19:04:27 +0700

    re-write sql to get Account and then set more parameter

--- MyAssignment/src/java/dal/AccountDBContext.java
+++ MyAssignment/src/java/dal/AccountDBContext.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Account;
+import model.Feature;
+import model.Role;
 
 /**
  *
@@ -20,18 +22,51 @@ public class AccountDBContext extends DBContext<Account> {
 
     public Account get(String email, String password) {
         try {
-            String sql = "SELECT email,username FROM Account\n"
-                    + "WHERE email = ? AND password = ?";
+            String sql = "SELECT a.email, a.username,\n"
+                    + "r.roid,r.roname,\n"
+                    + "f.fid,f.fname,f.furl\n"
+                    + "FROM Account a\n"
+                    + "LEFT JOIN Role_Account ra ON a.email = ra.email\n"
+                    + "LEFT JOIN [Role] r ON r.roid = ra.roid\n"
+                    + "LEFT JOIN Role_Feature rf ON rf.roid = r.roid\n"
+                    + "LEFT JOIN Feature f ON f.fid = rf.fid\n"
+                    + "WHERE a.email = ? AND a.password = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, email);
             stm.setString(2, password);
             ResultSet rs = stm.executeQuery();
-            if (rs.next()) {
-                Account account = new Account();
-                account.setEmail(rs.getString("email").toLowerCase());
-                account.setUsername(rs.getString("username"));
-                return account;
+            Account account = null;
+            Role currentRole = new Role();
+            currentRole.setId(-1);
+            while (rs.next()) {
+                if (account == null) {
+                    account = new Account();
+                    account.setEmail(rs.getString("email"));
+                    account.setUsername(rs.getString("username"));
+                }
+                int rid = rs.getInt("roid");
+                if(rid!=0)
+                {
+                    if(rid!= currentRole.getId())
+                    {
+                        currentRole = new Role();
+                        currentRole.setId(rs.getInt("roid"));
+                        currentRole.setName(rs.getString("roname"));
+                        account.getRoles().add(currentRole);
+                    }
+                }
+                
+                int fid = rs.getInt("fid");
+                if(fid!=0)
+                {
+                    Feature f = new Feature();
+                    f.setId(rs.getInt("fid"));
+                    f.setName(rs.getString("fname"));
+                    f.setUrl(rs.getString("furl"));
+                    currentRole.getFeatures().add(f);
+                }
             }
+            return account;
         } catch (SQLException ex) {
             Logger.getLogger(AccountDBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
